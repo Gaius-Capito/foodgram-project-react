ae07f6d0432e3a2728ed95ee9b3bf064023f7f50
Владислав Бунин <gaius.capito@gmail.com>
2023-08-22 12:04:15 +0300

    На ревью (Уточнение в settings.py / HOSTS)

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG', default=True)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='localhost').split()
 
 INSTALLED_APPS = [
     'django.contrib.admin',
